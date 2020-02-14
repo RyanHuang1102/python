ad8d60986b3d9582c6a6be4b722f375c20066fe6
ryanuhang <ryanhuang@qnap.com>
2020-02-14 17:48:30 +0800

    add qnap_cfg to set setcfg/get/rm

---
+++ QCS/log.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import logging
+from logging.handlers import RotatingFileHandler
+#1 Mbytes
+class LOG():
+    def __init__(self, file_path):
+        self.logger = logging.getLogger()
+        self.handler = RotatingFileHandler(file_path, maxBytes = 1024, backupCount = 1)
+        self.formatter = logging.Formatter(fmt = '%(asctime)s %(levelname)s %(message)s', datefmt = '%a,%d %b,%Y %H:%M:%S')
+        self.handler.setFormatter(self.formatter)
+        self.logger.addHandler(self.handler)
+
+    def CRITICAL_TRACE(self, string):
+        self.logger.setLevel(logging.CRITICAL)
+        self.logger.info(string)
+    def ERROR_TRACE(self, string):
+        self.logger.setLevel(logging.ERROR)
+        self.logger.info(string)
+
+    def WARNING_TRACE(self, string):
+        self.logger.setLevel(logging.WARNING)
+        self.logger.info(string)
+
+    def INFO_TRACE(self, string):
+        self.logger.setLevel(logging.INFO)
+        self.logger.info(string)
+
+    def DEBUG_TRACE(self, string):
+        self.logger.setLevel(logging.DEBUG)
+        self.logger.info(string)
+
+LOG = LOG('my.log')
+
+a=0
+while a<40:
+    LOG.INFO_TRACE("haha"+str(a))
+    a+=1
